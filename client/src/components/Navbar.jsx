e8d0344a505a5a4057d8d62f4f7c4ea349e0742f
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-12 13:37:29 -0800

    changed title

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,13 +3,10 @@ import  { Link, NavLink, withRouter } from 'react-router-dom';
 
 
 const Navbar = () => {
-    setTimeout(() => {
-    props.history.push('./about')
-  }, 2000)
   return(
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Poke Times</a>
+        <a className="brand-logo">React/Redux</a>
         <ul className="right">
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
