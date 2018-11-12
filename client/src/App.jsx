e418ca60cdaf3d024486ccec04818829df0f61a9
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-12 14:18:30 -0800

    set up route params

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import Contact from './components/Contact.jsx';
+import Post from './components/Post.jsx';
+
 
 
 class App extends Component {
@@ -15,6 +17,8 @@ class App extends Component {
           <Route exact path='/' component={ Home }/>
           <Route path='/about' component={ About }/>
           <Route path='/contact' component={ Contact }/>
+          {/* Route paramter*/}
+          <Route path="/:post_id" component={Post} />
         </div>
       </BrowserRouter>
     );
