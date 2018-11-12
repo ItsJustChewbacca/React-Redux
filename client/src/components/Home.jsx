8943a553ae90c5782742b036973b92feba87869b
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-12 14:10:01 -0800

    using lifecycle hook tograb dummy data

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 
-const Home = () => {
-  return(
-    <div className="container">
-      <h4 className="center">Home</h4>
+class Home extends Component {
+  state = {
+    posts: [ ]
+  }
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log(res)
+        this.setState({
+          posts: res.data.slice(0, 10)
+        })
+      })
+  }
 
-    </div>
-  )
+  render() {
+    const { posts } = this.state
+    const postList = !posts.length  ? (
+      <div className="center"> No posts yet</div>
+    ) : (
+      posts.map(post => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        )
+      })
+    )
+    return(
+      <div className="container">
+        <h4 className="center">Home</h4>
+        { postList }
+      </div>
+    )
+  }
 }
 
 export default Home
