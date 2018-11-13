fcf6661e1bb2d528f56401e88f281d1c63f0378b
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-13 00:29:32 -0800

    setting up redux store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
+//import redux store
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import {rootReducer} from './reducers/rootReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Creating store and passing ruducer function
+const store = createStore(rootReducer)
+
+//Wrapping react application with react-redux provider
+//This allows react to access the store
+ReactDOM.render(<Provider store= {store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
