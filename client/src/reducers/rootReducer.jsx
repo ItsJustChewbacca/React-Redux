b8262ee06ad7fda5243b95c2be314813810838c0
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-13 15:55:45 -0800

    created reducer

--- client/src/reducers/rootReducer.jsx
+++ client/src/reducers/rootReducer.jsx
@@ -1,7 +1,11 @@
 const initState = {
-  posts: []
+  posts: [
+   {id: "1", title: "Luke", body: "Programmer"},
+   {id: "2", title: "Sarah", body: "Student"},
+   {id: "3", title: "Sarbear", body: "MehMehMehMehMeh"}
+  ]
 }
-
+// Settting up reducer
 const rootReducer = (state = initState, action) => {
   return state;
 }
