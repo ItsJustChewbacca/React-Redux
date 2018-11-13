e58a3714ada29bd93ebb0c4ee133643a7f211656
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-13 15:55:27 -0800

    added reducer for redux store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,16 +6,17 @@ import * as serviceWorker from './serviceWorker';
 //import redux store
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import {rootReducer} from './reducers/rootReducer'
+import rootReducer from './reducers/rootReducer.jsx'
 
 //Creating store and passing ruducer function
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
 //Wrapping react application with react-redux provider
 //This allows react to access the store
-ReactDOM.render(<Provider store= {store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
