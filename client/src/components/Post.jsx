b65720d7891707562b905686f7fffe6bc01ca76f
Luke Baziw-Grossmann <ItsJustChewbacca@gmail.com>
2018-11-13 16:13:06 -0800

    action creator

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import { connect } from 'react-redux';
-
+import { deletePost } from '../actions/postActions.jsx'
 
 class Post extends Component {
 //Dont need with redux
@@ -64,7 +64,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    deletePost: (id) => {dispatch({type: 'DELETE_POST', id})}
+    deletePost: (id) => {dispatch(deletePost(id))}
   }
 }
 
